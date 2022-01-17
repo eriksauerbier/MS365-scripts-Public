1e9ff1d8d39b64e020d5ced315bdb9f183e65aa5
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-01-17 13:42:01 +0100

    1.12
    - Skript setzt nun nur noch die Regional-Einstellungen bei Postfächer, die abweichend sind
    - Regional-Einstellungen mittels Parameter definiert

--- Set-ExchangeOnlineMailboxFolder-to-German.ps1
+++ Set-ExchangeOnlineMailboxFolder-to-German.ps1
@@ -1,21 +1,26 @@
 ﻿# Skript zur Anpassung der ExchangeOnline Ordner-Namen
-# Stannek GmbH  v.1.11 - 28.12.2021 - E.Sauerbier
+# Stannek GmbH  v.1.12 - 17.01.2022 - E.Sauerbier
 
 # Diese Skript muss als Administrator ausgeführt werden, ansonsten wird es nicht gestartet
 #Requires -RunAsAdministrator
+
+#Parameter
+$Language = "de-DE"
+$DateFormat = "dd.MM.yyyy"
+$TimeFormat = "HH:mm"
+$TimeZone = "W. Europe Standard Time"
 
 If (!(Get-Module ExchangeOnlineManagement)) {Install-Module -Name ExchangeOnlineManagement}
 import-Module ExchangeOnlineManagement
 
 Connect-ExchangeOnline -ErrorAction Stop
 
-# Get-Mailbox | set-MailboxRegionalConfiguration -LocalizeDefaultFolderName:$true -Language de-DE -DateFormat "dd.MM.yyyy"
+# Alle Benutzer Postfächer auslesen und Regional-Konfiguration anpassen
 
-Get-Mailbox -ResultSize unlimited | ? {$_.RecipientTypeDetails -eq "UserMailbox"} | Set-MailboxRegionalConfiguration -Language "de-DE" -DateFormat "dd.MM.yyyy" -TimeFormat "HH:mm" -TimeZone "W. Europe Standard Time" -LocalizeDefaultFolderName
-Get-Mailbox -ResultSize unlimited | ? {$_.RecipientTypeDetails -eq "SharedMailbox"} | Set-MailboxRegionalConfiguration -Language "de-DE" -DateFormat "dd.MM.yyyy" -TimeFormat "HH:mm" -TimeZone "W. Europe Standard Time" -LocalizeDefaultFolderName
+$ChangedMailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox, SharedMailbox, RoomMailbox, EquipmentMailbox | Get-MailboxRegionalConfiguration | Where-Object {$_.Language -ne $Language}
+$ChangedMailboxes | Set-MailboxRegionalConfiguration -Language $Language -DateFormat $DateFormat -TimeFormat $TimeFormat -TimeZone $TimeZone -LocalizeDefaultFolderName
 
 
 # Ergebnis anzeigen
-Get-Mailbox -ResultSize unlimited | ? {$_.RecipientTypeDetails -eq "UserMailbox"} | Get-MailboxRegionalConfiguration
-Get-Mailbox -ResultSize unlimited | ? {$_.RecipientTypeDetails -eq "SharedMailbox"} | Get-MailboxRegionalConfiguration
-
+Write-host "Folgende Postfächer wurden angepasst: " $ChangedMailboxes.Identity
+Read-Host "Zum beenden beliebige Taste drücken"
