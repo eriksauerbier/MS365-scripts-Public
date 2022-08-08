6aab9501dd56e0016265cce62464c70a44c1d796
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-08-08 12:28:32 +0200

    v.1.11
    - Skript-Kommentare angepasst
    - weitere Lösch-Optionen hinzugefügt

--- MigrateOutlooktoMS365.bat
+++ MigrateOutlooktoMS365.bat
@@ -1,21 +1,27 @@
 rem Skript für die Migration des Outlook-Profils von OnPrem Exchange zu MS365
-rem Stannek GmbH v.1.1 - 23.03.2022 - E.Sauerbier
+rem Stannek GmbH v.1.11 - 08.08.2022 - E.Sauerbier
 
+rem #########
 rem Parameter
 
 SET OutlookPath="C:\Program Files (x86)\Microsoft Office\root\Office16\Outlook.exe"
 SET OSTPath1="%LocalAppData%\Microsoft\Outlook\*OST"
 SET OSTPath2="%HomeDrive%\Outlook\*OST"
+SET OutlookRegPath="HKCU\Software\Microsoft\Office\16.0\Outlook"
+SET OutlookRegCopyPath="HKCU\Software\Microsoft\Office\16.0\Outlook.Old"
 SET OutlookORGProfilePath="HKCU\Software\Microsoft\Office\16.0\Outlook\Profiles"
 SET OutlookCopyProfilePath="HKCU\Software\Microsoft\Office\16.0\Outlook\Profiles.OnPREM"
 SET OutlookAutoDicoverPath="HKCU\Software\Microsoft\Office\16.0\Outlook\Autodiscover"
+rem #########
 
-rem Outlook starten um aktuelle Einstellungen zu prüfen
+rem #####################################
+rem Outlook starten um aktuelle Einstellungen zu prüfen, Zeile bei Bedarf auskommentieren
 
-call %OutlookPath% 
+rem call %OutlookPath% 
 
-pause
+rem pause
 
+rem #####################################
 rem vorhandene OST-Dateien suchen und löschen
 
 if Exist %OSTPath1% (del %OSTPath1%)
@@ -23,13 +29,26 @@ if Exist %OSTPath2% (del %OSTPath2%)
 
 pause
 
-rem Outlook-Profil löschen
+rem #####################################
+rem AutoDiscover Einstellung zurücksetzen
 
-reg copy %OutlookORGProfilePath% %OutlookCopyProfilePath% /s
-reg Delete %OutlookORGProfilePath% /f
+reg Delete %OutlookAutoDicoverPath% /f
+
+rem #####################################
+rem Outlook-Profil löschen oder den komplette Outlook-Einstellungenen inkl. Profile löschen
+rem Entsprechend auskommentieren
+
+rem komplette Outlook-Einstellungenen inkl. Profile 
+reg copy %OutlookRegPath% %OutlookRegCopyPath% /s
+reg Delete %OutlookRegPath% /f
+
+rem Nur Outlook-Profil
+rem reg copy %OutlookORGProfilePath% %OutlookCopyProfilePath% /s
+rem reg Delete %OutlookORGProfilePath% /f
 
 pause
 
+rem #####################################
 rem AutoDiscover Einstellung zurücksetzen
 
 reg Delete %OutlookAutoDicoverPath% /f
@@ -40,6 +59,7 @@ control keymgr.dll
 
 pause
 
+rem ###############
 rem Outlook starten
 
 call %OutlookPath% 
