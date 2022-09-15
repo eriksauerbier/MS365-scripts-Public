5c4e3a734929af97d274ba200784b64b760c848d
eriksauerbier <erik.sauerbier@mailbox.org>
2022-09-15 08:03:37 +0200

    v.1.3.2

--- MigrateOutlooktoMS365.bat
+++ MigrateOutlooktoMS365.bat
@@ -1,7 +1,7 @@
 rem Skript für die Migration des Outlook-Profils von OnPrem Exchange zu MS365
-rem Stannek GmbH v.1.3.1 - 29.08.2022 - E.Sauerbier
+rem Stannek GmbH v.1.3.2 - 15.09.2022 - E.Sauerbier
 
-rem Parameter
+rem ## Parameter ##
 
 SET OutlookPath="C:\Program Files (x86)\Microsoft Office\root\Office16\Outlook.exe"
 SET OSTPath1="%LocalAppData%\Microsoft\Outlook\*OST"
@@ -10,12 +10,15 @@ SET OutlookORGProfilePath="HKCU\Software\Microsoft\Office\16.0\Outlook\Profiles"
 SET OutlookCopyProfilePath="HKCU\Software\Microsoft\Office\16.0\Outlook\Profiles.OnPREM"
 SET OutlookAutoDicoverPath="HKCU\Software\Microsoft\Office\16.0\Outlook\Autodiscover"
 SET OutlookSettingsPath="HKCU\Software\Microsoft\Office\Outlook\Settings"
+SET OutlookPath="HKCU\Software\Microsoft\Office\16.0\Outlook"
+SET OutlookCopyPath="HKCU\Software\Microsoft\Office\16.0\Outlook.OnPREM"
+
+rem #################
 
 rem Outlook starten um aktuelle Einstellungen zu prüfen
+rem call %OutlookPath% 
 
-call %OutlookPath% 
-
-pause
+rem pause
 
 rem vorhandene OST-Dateien suchen und löschen
 
@@ -26,15 +29,15 @@ pause
 
 rem Outlook-Profil und Einstellungen löschen
 
-reg copy %OutlookORGProfilePath% %OutlookCopyProfilePath% /s
-reg Delete %OutlookORGProfilePath% /f
+reg copy %OutlookPath% %OutlookCopyPath% /s
+reg Delete %OutlookPath% /f
 reg Delete %OutlookSettingsPath% /f
 
 pause
 
-rem AutoDiscover Einstellung zurücksetzen
+rem AutoDiscover Einstellung zurücksetzen (Nur dann Notwendig, wenn nicht der Komplette Outlook-Zweig gelöscht wird)
 
-reg Delete %OutlookAutoDicoverPath% /f
+rem reg Delete %OutlookAutoDicoverPath% /f
 
 rem DATEV Office-Einstellung zurücksetzen
 
