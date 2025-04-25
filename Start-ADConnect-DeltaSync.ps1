8e3eff72a9d00c9b3a705d2c543f0d0dd965951d
eriksauerbier <erik.sauerbier@mailbox.org>
2025-04-25 13:45:09 +0200

    .

--- Start-ADConnect-DeltaSync.ps1
+++
@@ -1,8 +0,0 @@
-# Skript zum manuellen synchronisieren von AzureAD Connect
-# Stannek GmbH v.1.01 - 28.12.2021 - E.Sauerbier
-
-# AADC Module laden
-Import-Module ADSync
-
-#Start-ADSyncSyncCycle
-Start-ADSyncSyncCycle -PolicyType Delta
